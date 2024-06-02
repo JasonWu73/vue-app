86afc73179cb55815f10cd8b209b5d3721e567d5
吴仙杰 <wuxianjiezh@gmail.com>
2024-06-02 23:13:48 +0800

    Lazy load

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,10 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
 
 import HomeView from './components/home/HomeView.vue';
-import AboutView from './components/about/AboutView.vue';
 
 const routes: Readonly<RouteRecordRaw[]> = [
   { path: '/', component: HomeView },
-  { path: '/about', component: AboutView }
+  { path: '/about', component: () => import('./components/about/AboutView.vue') }
 ];
 
 const router = createRouter({
