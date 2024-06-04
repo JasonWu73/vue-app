8c58e88ea5c1173fe0f061aafa11663076d8a374
吴仙杰 <wuxianjiezh@gmail.com>
2024-06-04 09:22:04 +0800

    增加 Tailwind 配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,33 @@
-/** @type {import("tailwindcss").Config} */
+import animatePlugin from 'tailwindcss-animate';
+
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}"
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}'
   ],
+
+  // 启用深色模式，通过在 HTML 元素上添加或移除 `dark` 类来手动切换深色模式
+  darkMode: 'class',
+
   theme: {
-    extend: {}
+    extend: {
+      height: {
+        // 考虑手机浏览器的地址栏
+        screen: ['100vh', '100dvh']
+      },
+
+      /* 添加新的颜色 */
+      colors: {
+        'one-dark': {
+          DEFAULT: 'rgb(var(--color-one-dark) / <alpha-value>)',
+          1: 'rgb(var(--color-one-dark-1) / <alpha-value>)',
+          2: 'rgb(var(--color-one-dark-2) / <alpha-value>)'
+        }
+      }
+    }
   },
-  plugins: []
+
+  plugins: [animatePlugin]
 };
 
